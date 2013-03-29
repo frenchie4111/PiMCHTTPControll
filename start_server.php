24573c5dca8e875463827e3b27b64fa3c851a139
Mike <mike@alaska-desktop.(none)>
2013-03-29 15:46:11 -0400

    Removed password from file

--- start_server.php
+++ start_server.php
@@ -1,3 +1,3 @@
 <?php
-	passthru('omxplayer --adev local "http://frenchie:michael9@bluefile.org:9090/Sweet%20Thing%20(CD%20-%20MP3%20-%20V0)/01%20-%20Sweet%20Thing%20-%20Change%20of%20Seasons.mp3" < /tmp/cmd');
+	passthru('omxplayer --adev local "http://mikelyons.org/external/01%20-%20Sweet%20Thing%20-%20Change%20of%20Seasons.mp3" < /tmp/cmd');
 ?>
